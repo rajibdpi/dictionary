775e5a20f1fac4264aba59f96d49a6bd6855d78c
Rajib Ahmed <rajibdpi@gmail.com>
2022-04-14 02:35:41 +0600

    Updated db format

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _HomePageState extends State<HomePage> {
         await rootBundle.loadString('assets/BengaliDictionary.json');
     final data = await json.decode(response);
     setState(() {
-      _items = data["words"];
+      _items = data['words'];
     });
   }
 
@@ -71,8 +71,35 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-      body: const Center(
-        child: Text('Open Source English to Bangla Dictionary'),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Load Data'),
+              onPressed: readJson,
+            ),
+
+            // Display the data loaded from sample.json
+            _items.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: const EdgeInsets.all(10),
+                          child: ListTile(
+                            leading: Text(_items[index]["bn"]),
+                            title: Text(_items[index]["en"]),
+                            // subtitle: Text(_items[index]["description"]),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Container()
+          ],
+        ),
       ),
     );
   }
