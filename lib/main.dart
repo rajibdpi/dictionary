68c2e3f8ebdee5dd924e8b8f5e70a1a40ca19df3
Rajib Ahmed <rajibdpi@gmail.com>
2022-04-15 00:03:54 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
 
 void main() {
@@ -18,14 +17,6 @@ class MyApp extends StatelessWidget {
       title: 'E2B Dictionary',
       theme: ThemeData(
         // This is the theme of the application.
-        //
-        // Try running application with "flutter run". You'll see the
-        // application has a teal toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
       ),
       home: const HomePage(
@@ -68,40 +59,36 @@ class _HomePageState extends State<HomePage> {
               print('Search Button Pressed');
             },
             icon: const Icon(Icons.search),
-          )
+          ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            ElevatedButton(
-              child: const Text('Load Data'),
-              onPressed: readJson,
-            ),
-
-            // Display the data loaded from sample.json
-            _items.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                      itemCount: _items.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          margin: const EdgeInsets.all(10),
-                          child: ListTile(
-                            leading: Text(_items[index]["en"]),
-                            title: Text(_items[index]["bn"]),
-                            subtitle:
-                                Text('en_syns-${_items[index]["en_syns"]} -- '
-                                    'bn_syns-${_items[index]["bn_syns"]}'),
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                : Container()
-          ],
-        ),
+      body: FutureBuilder(
+        future: readJson(),
+        builder: (context, dynamic) {
+          if (_items.isNotEmpty) {
+            return ListView.builder(
+              itemCount: _items.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: Text(_items[index]["en"]),
+                    title: Text(_items[index]["bn"]),
+                    subtitle: Text('en_syns-${_items[index]["en_syns"]} -- '
+                        'bn_syns-${_items[index]["bn_syns"]}'),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [CircularProgressIndicator(), Text('Loading')],
+              ),
+            );
+          }
+        },
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dictionary/main.dart';
+import 'package:dictionary/main.txt';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
